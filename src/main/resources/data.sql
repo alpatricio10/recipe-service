40b646eaaf0e47cebf800fdfe2871a26a94cbff6
Adrian Patricio <adrian.patricio@paymaya.com>
2023-12-17 01:53:33 +0800

    Create initialization script for H2 for local testing

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO recipe (name, image_url, difficulty, position) VALUES
+    ('Recipe1', 'https://ddg0cip9uom1w.cloudfront.net/code-challenge/burger.jpg', 'EASY', 1),
+    ('Recipe2', 'https://ddg0cip9uom1w.cloudfront.net/code-challenge/burger.jpg', 'MEDIUM', 2),
+    ('Recipe3', 'https://ddg0cip9uom1w.cloudfront.net/code-challenge/burger.jpg', 'HARD', 3),
+    ('Recipe4', 'https://ddg0cip9uom1w.cloudfront.net/code-challenge/burger.jpg', 'EASY', 6),
+    ('Recipe5', 'https://ddg0cip9uom1w.cloudfront.net/code-challenge/burger.jpg', 'HARD', 5),
+    ('Recipe6', 'https://ddg0cip9uom1w.cloudfront.net/code-challenge/burger.jpg', 'EASY', 4);
