962cbd8afad80c7d39053d781a4de850d3763079
Adrian Patricio <adrian.patricio@paymaya.com>
2023-12-17 11:32:44 +0800

    Add timestamp and updater fields to Recipe entity

--- src/main/java/com/recime/recipeservice/data/Recipe.java
+++ src/main/java/com/recime/recipeservice/data/Recipe.java
@@ -7,8 +7,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @Data
 @Entity
@@ -30,6 +37,20 @@ public class Recipe {
     private DifficultyType difficulty;
 
     private int position;
+
+    @CreationTimestamp
+    @DateTimeFormat(pattern="YYYY-MM-dd HH:mm:ss")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTimestamp;
+
+    private String createdBy;
+
+    @UpdateTimestamp
+    @DateTimeFormat(pattern="YYYY-MM-dd HH:mm:ss")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateTimestamp;
+
+    private String updatedBy;
 
     public Recipe(String name, String imageUrl, DifficultyType difficulty, int position) {
         this.name = name;
